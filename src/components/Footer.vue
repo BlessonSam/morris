68d4dd57ed902e60d3ccf4bbfd7f3bbfcfbfc9f3
blesson2 <blesson@mounty.co>
2022-01-01 18:55:29 +0530

    - added logo in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,10 @@
 <template>
   <div dark class="h-40 text-gray-400" style="background-color: #001123ad; backdrop-filter:blur(7px); -webkit-backdrop-filter: blur(7px);">
       <div class="container mx-auto flex flex-center fit ">
-        <div class="text-center  space-y-3">
+        <div class="text-center space-y-5">
+           <img src="../assets/morris_logo.jpeg" class="w-10 h-10 mx-auto"/>
            <div> Copyright Morris English Academy &copy; {{new Date().getFullYear()}}. All Rights Reserved </div>
-             {{$route.name}}
+             
         </div>
              
       </div>
