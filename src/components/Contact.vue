d593d11bab8fad4ddb7a207ef75cd0c6712aafe1
blesson2 <blesson@mounty.co>
2022-02-01 07:06:48 +0530

    - api integration

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,73 +1,134 @@
 <template>
-    <q-page dark class="container pt-20 pb-10 space-y-20 text-center">
+  <q-page dark class="container pt-20 pb-10 space-y-20 text-center">
+    <div class="text-4xl text-center text-brand">Contact Us</div>
 
-         <div class="text-4xl  text-center text-bigText">Contact Us</div>
+    <div
+      class="w-full flex flex-wrap space-y-20 lg:space-y-0 lg:flex-nowrap lg:space-x-5 lg:divide-x divide-gray-700"
+    >
+      <div class="space-y-10 w-full lg:mr-5">
+        <div class="text-xl font-semibold text-sectionSubTitle">
+          Drop us a line!
+        </div>
+        <div class="mx-auto space-y-5" style="max-width: 500px">
+          <q-input
+            dark
+            color="sectionSubTitle"
+            rounded
+            outlined
+            v-model="name"
+            label="Full Name"
+          />
+          <q-input
+            dark
+            color="sectionSubTitle"
+            v-model="email"
+            rounded
+            outlined
+            type="email"
+            label="Email"
+          />
+          <q-input
+            dark
+            color="sectionSubTitle"
+            v-model="phone"
+            rounded
+            outlined
+            type="tel"
+            label="Phone Number"
+          />
+          <q-input
+            dark
+            color="sectionSubTitle"
+            v-model="message"
+            rounded
+            outlined
+            type="textarea"
+            label="Message"
+          />
+          <q-btn
+            dark
+            @click="submitClicked"
+            rounded
+            no-caps
+            color="sectionSubTitle"
+            class="text-black"
+            >Send Message</q-btn
+          >
+        </div>
+      </div>
 
-         <div class="w-full flex flex-wrap space-y-20 lg:space-y-0 lg:flex-nowrap lg:space-x-5 lg:divide-x divide-gray-700"> 
-          <div class="space-y-10 w-full lg:mr-5">
-              <div class="text-xl font-semibold text-sectionSubTitle">Drop us a line!</div>
-              <div class="mx-auto space-y-5" style="max-width:500px">
-                 <q-input dark color="sectionSubTitle" rounded outlined v-model="name" label="Full Name" />
-                 <q-input dark color="sectionSubTitle" v-model="email" rounded outlined type="email" label="Email" />
-                 <q-input dark color="sectionSubTitle" v-model="phone" rounded outlined type="tel" label="Phone Number" />
-                 <q-input dark color="sectionSubTitle" v-model="message" rounded outlined type="textarea" label="Message" />
-                 <q-btn dark @click="submitClicked" rounded color="sectionSubTitle" class="text-black">Send Message</q-btn>
-              </div>
+      <div class="space-y-10 w-full">
+        <div class="text-xl font-semibold text-sectionSubTitle">
+          Better yet, see us in person!
+        </div>
+        <div class="mx-auto space-y-5" style="max-width: 500px">
+          <div class="space-y-2 text-body">
+            <div class="font-semibold">{{ address.firstLine }}</div>
+            <div v-for="line in address.lines" :key="line">{{ line }}</div>
           </div>
-
-
-
-         <div class="space-y-10 w-full">
-              <div class="text-xl font-semibold text-sectionSubTitle">Better yet, see us in person!</div>
-               <div class="mx-auto space-y-5" style="max-width:500px">
-                  <div class="space-y-2 text-body">
-                      <div class="font-semibold">{{address.firstLine}}</div>
-                      <div v-for="line in address.lines" :key="line">{{line}}</div>
-                  </div>
-                  <div class="text-body">{{address.email}}</div>
-                  <div class="space-y-2 text-body">
-                     <div v-for="number in address.phoneNumbers" :key="number" class="">{{number}}</div>
-                  </div>
-                 
-              </div>
+          <div class="text-body">{{ address.email }}</div>
+          <div class="space-y-2 text-body">
+            <div v-for="number in address.phoneNumbers" :key="number" class="">
+              {{ number }}
+            </div>
           </div>
-         </div>
-
-    </q-page>
+        </div>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
-export default {
-    setup() {
-        
-    },
-    data(){
-        return {
-            name:"",
-            email:"",
-            message:"",
-            phone:"",
-            address:{
-                firstLine:"Morris Anglo-Indian English Academy",
-                lines:[
-                    "TC 13/175/1, ARCANGELS",
-                    "NALUMUKKU JUNCTION ,Pattoor –Airport Road",
-                    "opposite HDFC /AXIS Bank ,Near H P Petrol Pump",
-                    "PETTAH,TRIVANDRUM-24"
-                ],
-                email:"contact@morrisacademy.com",
-                phoneNumbers:[
-                    "9745251362",
-                    "9349366924"
-                ]
-            }
+import { api } from "boot/axios";
+import { ref } from "vue";
+import { useQuasar } from "quasar";
 
-        }
-    },
-    methods:{
-        submitClicked(){
-           this.name = this.email= this.message = this.phone = ""
-        }
+export default {
+  setup() {
+    const $q = useQuasar();
+    const data = ref(null);
+
+    function loadData() {
+      api
+        .get("/courses/")
+        .then((response) => {
+          data.value = response.data;
+        })
+        .catch(() => {
+          $q.notify({
+            color: "negative",
+            position: "top",
+            message: "Loading failed",
+            icon: "report_problem",
+          });
+        });
     }
-}
+
+    return { data, loadData };
+  },
+  data() {
+    return {
+      name: "",
+      email: "",
+      message: "",
+      phone: "",
+      address: {
+        firstLine: "Morris Anglo-Indian English Academy",
+        lines: [
+          "TC 13/175/1, ARCANGELS",
+          "NALUMUKKU JUNCTION ,Pattoor –Airport Road",
+          "opposite HDFC /AXIS Bank ,Near H P Petrol Pump",
+          "PETTAH,TRIVANDRUM-24",
+        ],
+        email: "morrisacademy@morrismorris.in",
+        phoneNumbers: ["9745251362", "9349366924"],
+      },
+    };
+  },
+  methods: {
+    submitClicked() {
+      this.name = this.email = this.message = this.phone = "";
+    },
+  },
+};
 </script>
--- src/pages/Courses.vue
+++ src/pages/Courses.vue
@@ -1,95 +1,171 @@
 <template>
-    <q-page dark class="py-10">
-       <div class="container text-center pb-10 pt-20 space-y-3">
-           <div class="text-semibold text-4xl text-bigText">Our English Learning Products</div>
-           <div class="text-xs text-sectionSubTitle">Our specialised programmes in ENGLISH </div>
-       </div>
-       <div class="container pt-10 grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 2xl:grid-cols-4 gap-12">
-            <div v-for="product in products" :key="product.title" dark class="m-3 md:m-5 lg:m-7 p-10 rounded-2xl flex content-between flex-wrap bg-dark  hover:shadow-2xl space-y-8 overflow-hidden" style="max-width:300px; min-width:200px; max-height:600px; min-height:300px;">
-           <div class="space-y-3 w-full overflow-hidden text-ellipsis" style="max-height:200px;">
-              <div class="text-2xl font-semibold cursor-pointer text-sectionTitle" @click="goToProduct(product.id)">{{product.title}}</div>
-              <div class="text-sm text-sectionSubTitle text-ellipsis overflow-hidden">{{product.content}}</div>
-           </div>
-           
-           <div class="flex justify-between w-full">
-             <div @click="goToProduct(product.id)" class="font-bold cursor-pointer text-sectionTitle">Know More</div>
-             <div @click="goToProduct(product.id)" class="text-brand cursor-pointer"><q-icon name="arrow_forward" size="sm" /></div>
-           </div>
-         </div>
-       </div>
+  <q-page dark class="py-10">
+    <div class="container text-center pb-10 pt-20 space-y-3">
+      <div class="text-semibold text-4xl text-bigText">
+        Our English Learning Products
+      </div>
+      <div class="text-xs text-sectionSubTitle">
+        Our specialised programmes in ENGLISH
+      </div>
+    </div>
+    <div
+      class="container pt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-12"
+    >
+      <div
+        v-for="product in data"
+        :key="product.title"
+        dark
+        class="m-3 md:m-5 lg:m-7 p-10 rounded-2xl flex content-between flex-wrap bg-dark hover:shadow-2xl space-y-8 overflow-hidden"
+        style="
+          max-width: 300px;
+          min-width: 200px;
+          max-height: 600px;
+          min-height: 300px;
+        "
+      >
+        <div
+          class="space-y-3 w-full overflow-hidden text-ellipsis"
+          style="max-height: 200px"
+        >
+          <div
+            class="text-2xl font-semibold cursor-pointer text-sectionTitle"
+            @click="goToProduct(product.id)"
+          >
+            {{ product.title }}
+          </div>
+          <div
+            class="text-sm text-sectionSubTitle text-ellipsis overflow-hidden"
+          >
+            {{ product.content }}
+          </div>
+        </div>
 
-       
+        <div class="flex justify-between w-full">
+          <div
+            @click="goToProduct(product.id)"
+            class="font-bold cursor-pointer text-sectionTitle"
+          >
+            Know More
+          </div>
+          <div
+            @click="goToProduct(product.id)"
+            class="text-brand cursor-pointer"
+          >
+            <q-icon name="arrow_forward" size="sm" />
+          </div>
+        </div>
+      </div>
+    </div>
 
-
-    </q-page>
+    {{ data }}
+  </q-page>
 </template>
 
 <script>
+import { api } from "boot/axios";
+import { ref } from "vue";
+import { useQuasar } from "quasar";
+
 export default {
+  setup() {
+    const $q = useQuasar();
+    const data = ref(null);
+
+    function loadData() {
+      api
+        .get("/courses/")
+
+        .then((response) => {
+          data.value = response.data;
+        })
+        .catch(() => {
+          $q.notify({
+            color: "negative",
+            position: "top",
+            message: "Loading failed",
+            icon: "report_problem",
+          });
+        });
+    }
+
+    return { data, loadData };
+  },
   data() {
-      return {
-           products:[
+    return {
+      products: [
         {
-          id:"dhgfgfd56456",
-          title:"IELTS (ONLINE AND CLASSROOM)",
-          content:"Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etsssssssssssss sssssssss ssssssssss ssssssssssssss sssssssssssss ssssssss sssssssssss sssssssss ssssss sssssssss ssss ssssss sssss ssss iam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla."
-        }, 
-        {
-          id:"dhgfgfd56456",
-          title:"OET (ONLINE AND CLASSROOM)",
-          content:"Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla."
-        },
-         {
-          id:"dhgfgfd56456",
-          title:"COMMUNICATON SKILLS",
-          content:"Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla."
+          id: "dhgfgfd56456",
+          title: "IELTS (ONLINE AND CLASSROOM)",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etsssssssssssss sssssssss ssssssssss ssssssssssssss sssssssssssss ssssssss sssssssssss sssssssss ssssss sssssssss ssss ssssss sssss ssss iam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla.",
         },
         {
-          id:"dhgfgfd56456",
-          title:"IELTS (ONLINE AND CLASSROOM)",
-          content:"Lorem ipsum dolor sit amet, consectetur adipis cingelit. tra vel nibh sit amet, tincidunt posuere nulla."
+          id: "dhgfgfd56456",
+          title: "OET (ONLINE AND CLASSROOM)",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla.",
         },
         {
-          id:"dhgfgfd56456",
-          title:"OET (ONLINE AND CLASSROOM)",
-          content:"Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla."
-        },
-         {
-          id:"dhgfgfd56456",
-          title:"COMMUNICATON SKILLS",
-          content:"Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla."
+          id: "dhgfgfd56456",
+          title: "COMMUNICATON SKILLS",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla.",
         },
         {
-          id:"dhgfgfd56456",
-          title:"IELTS (ONLINE AND CLASSROOM)",
-          content:"Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla."
+          id: "dhgfgfd56456",
+          title: "IELTS (ONLINE AND CLASSROOM)",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipis cingelit. tra vel nibh sit amet, tincidunt posuere nulla.",
         },
         {
-          id:"dhgfgfd56456",
-          title:"OET (ONLINE AND CLASSROOM)",
-          content:"Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla."
+          id: "dhgfgfd56456",
+          title: "OET (ONLINE AND CLASSROOM)",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla.",
         },
-         {
-          id:"dhgfgfd56456",
-          title:"COMMUNICATON SKILLS",
-          content:"Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla."
+        {
+          id: "dhgfgfd56456",
+          title: "COMMUNICATON SKILLS",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla.",
+        },
+        {
+          id: "dhgfgfd56456",
+          title: "IELTS (ONLINE AND CLASSROOM)",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla.",
+        },
+        {
+          id: "dhgfgfd56456",
+          title: "OET (ONLINE AND CLASSROOM)",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla.",
+        },
+        {
+          id: "dhgfgfd56456",
+          title: "COMMUNICATON SKILLS",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipis cingelit. Etiam lacinia elit et placerat finibus. Praesent justo metus, pharetra vel nibh sit amet, tincidunt posuere nulla.",
         },
       ],
-      }
+    };
   },
-  methods:{
-      goToProduct(productId){
+  methods: {
+    goToProduct(productId) {
       this.$router.push({
-        path:`/courses/${productId}`
-      })
+        path: `/courses/${productId}`,
+      });
     },
-  }
-}
+  },
+
+  mounted() {
+    this.loadData();
+  },
+};
 </script>
 
 <style>
 .totoptransparent-bg {
   background-image: linear-gradient(to bottom, rgba(245, 245, 248, 0), #000c19);
 }
-
 </style>
